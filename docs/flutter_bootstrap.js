670e80b244e32328650044f5e58f476c16976eea
Philip Knoetze <pknoetze@gmail.com>
2024-11-30 21:45:08 +0000

    Deploy app to docs folder for GitHub Pages
    - Added production build to docs folder
    - Updated deployment configuration for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1196048494"
+    serviceWorkerVersion: "2638365184"
   }
 });
